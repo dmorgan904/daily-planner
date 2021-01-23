10d7e8c2e7841291909210360dc1bdcdff4a6f69
dmorgan904 <demetriusimorgan@gmail.com>
2021-01-23 18:23:24 -0500

    add save icon, make timeblock editable

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,23 @@
 //var date = new Date();
 //document.getElementById("currentDay").innerHTML = date; 
 
-// Display current date and time
+// Display current day
 var date = new Date();
-moment().format("MMMM Do, YYYY - hh:mm:ss a");
+moment().format('MMMM Do YYYY, h:mm:ss a');
 document.getElementById("currentDay").innerHTML = date; 
 
+/*document.querySelector(".info").addEventListener("click", function(event) {
+    if (event.target.matches(".info-item")) {
+        console.log("You did it!");
+    }
+}) */
+
+/*$(".info").on("click", ".info-item", function() {
+    console.log("You did it!");
+}); */
+
+$(".info").on("click", ".info-item", function() {
+    var text = $(this).text();
+    var textInput = $("textarea").addClass("")
+    console.log(text);
+}); 
