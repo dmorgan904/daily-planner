ce40667924a3515adbe0936b019c649deffe79c1
dmorgan904 <demetriusimorgan@gmail.com>
2021-01-23 16:48:13 -0500

    create js file, display time at top

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+//var date = new Date();
+//document.getElementById("currentDay").innerHTML = date; 
+
+// Display current date and time
+var date = new Date();
+moment().format("MMMM Do, YYYY - hh:mm:ss a");
+document.getElementById("currentDay").innerHTML = date; 
+
