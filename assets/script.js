110393cac6f09ddd32670a6fba99d00895ac3fcb
dmorgan904 <demetriusimorgan@gmail.com>
2021-01-23 21:25:36 -0500

    change time

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,33 @@
-//var date = new Date();
-//document.getElementById("currentDay").innerHTML = date; 
-
 // Display current day
-var date = new Date();
-moment().format('MMMM Do YYYY, h:mm:ss a');
-document.getElementById("currentDay").innerHTML = date; 
+/* var date = function() {
+    date = moment();
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+}; */
 
-/*document.querySelector(".info").addEventListener("click", function(event) {
-    if (event.target.matches(".info-item")) {
-        console.log("You did it!");
-    }
-}) */
+var updateInfo = function () {
+    date = moment();
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function() {
+    datetime = $('#currentDay')
+    updateInfo();
+    setInterval(updateInfo, 1000);
+    refresh();
+}); 
+
+var refresh = function() {
+
+}
+/*moment().format('MMMM Do YYYY, h:mm:ss a');
+document.getElementById("currentDay").innerHTML = date; */
+
+var currentHour = moment().format("H"); 
 
 /*$(".info").on("click", ".info-item", function() {
     console.log("You did it!");
 }); */
 
-$(".info").on("click", ".info-item", function() {
-    var text = $(this).text();
-    var textInput = $("textarea").addClass("")
-    console.log(text);
-}); 
+/*$(document).ready(function() {
+    date = 
+}) */
